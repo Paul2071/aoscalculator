1d4775d816d104ce925303a65fc13db7141a4454
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-04-18 17:47:25 +0100

    started adding top half of content to average dice rolls page.

--- pages/avgDice.js
+++ pages/avgDice.js
@@ -1,10 +1,47 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, TouchableOpacity, Image, StyleSheet } from 'react-native'
+import { TextInput } from 'react-native-paper'
 
 function AvgDice() {
   return (
-    <Text>avgDice</Text>
+    <View style={styles.mainContainer}>
+        <View style={styles.titlesAndInputContainer}>
+            <Text>How many dice?</Text>
+            <TextInput
+            keyboardType='numeric'
+            placeholder='0'>
+
+            </TextInput>
+            <TouchableOpacity
+            onPress={() => console.log("I am working")}>
+                <Image 
+                style={styles.imageResizing}
+                source={require('../assets/d6blackred.png')}>
+
+                </Image>
+            </TouchableOpacity>
+            <Text>Click to calculate averages</Text>
+
+
+        </View>
+
+
+    </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+mainContainer: {
+  flex: 1,
+},
+titlesAndInputContainer:{
+flex: 1,
+},
+imageResizing: {
+  height: 140,
+  width: 140,
+}
+})
 
 export default AvgDice
