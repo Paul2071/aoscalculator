ed8ea7f0d65e780c31f6b15abf30eade03bd86a7
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-04-19 08:19:43 +0100

    added state to track user input on avgdice page

--- pages/avgDice.js
+++ pages/avgDice.js
@@ -3,17 +3,22 @@ import { Text, View, TouchableOpacity, Image, StyleSheet } from 'react-native'
 import { TextInput } from 'react-native-paper'
 
 function AvgDice() {
+
+const [ howManyDice, setHowManyDice ] = React.useState("")
+
   return (
     <View style={styles.mainContainer}>
         <View style={styles.titlesAndInputContainer}>
             <Text>How many dice?</Text>
             <TextInput
+            value={howManyDice}
+            onChangeText={setHowManyDice}
             keyboardType='numeric'
             placeholder='0'>
 
             </TextInput>
             <TouchableOpacity
-            onPress={() => console.log("I am working")}>
+            onPress={() => console.log(howManyDice)}>
                 <Image 
                 style={styles.imageResizing}
                 source={require('../assets/d6blackred.png')}>
