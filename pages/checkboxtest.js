f1110da042944b505820d8887711b93bc2edc3fe
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-04-26 14:52:08 +0100

    sorted out positioning to my liking, a reminder to remove stlying from examples copy and pasted in. took far too long to find where the margin was coming from!

--- pages/checkboxtest.js
+++ pages/checkboxtest.js
@@ -8,97 +8,138 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
     const [isAlsoChecked, setisAlsoChecked] = useState(false);
   
     return (
-      <View style={styles.container}>
+      <View style={styles.container}>   
 
-        <View>
-            <Checkbox 
-              style={styles.checkbox} 
-              value={isChecked} 
-              onValueChange={setChecked} />
-            <Checkbox 
-              style={styles.checkbox} 
-              value={isChecked} 
-              onValueChange={setChecked} />
-            <Checkbox 
-              style={styles.checkbox} 
-              value={isChecked} 
-              onValueChange={setChecked} />
-            <Checkbox 
-              style={styles.checkbox} 
-              value={isChecked} 
-              onValueChange={setChecked} />
-            <Checkbox 
-              style={styles.checkbox} 
-              value={isChecked} 
-              onValueChange={setChecked} />
-            <Checkbox 
-              style={styles.checkbox} 
-              value={isChecked} 
-              onValueChange={setChecked} />
-            
-            
-         </View>
           <View>
-              <View style={styles.section}>
-                <Checkbox 
-                style={styles.checkbox} 
-                value={isChecked} 
-                onValueChange={setChecked} />
-                <Text style={styles.paragraph}>Reroll 1s</Text>
-              </View>
+                  <View style={styles.section}>
+                    <Checkbox 
+                    style={styles.checkbox} 
+                    value={isChecked} 
+                    onValueChange={setChecked} />
+                    <Text style={styles.paragraph}>Reroll 1s</Text>
+                    <Checkbox 
+                    style={styles.checkbox} 
+                    value={isChecked} 
+                    onValueChange={setChecked} />
+                  </View>
 
-              <View style={styles.section}>
-                <Checkbox 
-                style={styles.checkbox} 
-                value={isAlsoChecked} 
-                onValueChange={setisAlsoChecked} />
-                <Text style={styles.paragraph}>Mortal on 6, sequence ends</Text>
-              </View>
+                  <View style={styles.section}>
+                    <Checkbox 
+                    style={styles.checkbox} 
+                    value={isAlsoChecked} 
+                    onValueChange={setisAlsoChecked} />
+                    <Text style={styles.paragraph}>Mortal on 6, sequence ends</Text>
+                    <Checkbox 
+                    style={styles.checkbox} 
+                    value={isChecked} 
+                    onValueChange={setChecked} />
+                  </View>
 
-              <View style={styles.section}>
-                <Checkbox 
-                style={styles.checkbox} 
-                value={isAlsoChecked} 
-                onValueChange={setisAlsoChecked} />
-                <Text style={styles.paragraph}>Mortal on 6, continue roll</Text>
-              </View>
+                  <View style={styles.section}>
+                    <Checkbox 
+                    style={styles.checkbox} 
+                    value={isAlsoChecked} 
+                    onValueChange={setisAlsoChecked} />
+                    <Text style={styles.paragraph}>Mortal on 6, continue roll</Text>
+                    <Checkbox 
+                    style={styles.checkbox} 
+                    value={isAlsoChecked} 
+                    onValueChange={setisAlsoChecked} />
+                  </View>
 
-              <View style={styles.section}>
-                <Checkbox 
-                style={styles.checkbox} 
-                value={isAlsoChecked} 
-                onValueChange={setisAlsoChecked} />
-                <Text style={styles.paragraph}>D3 Mortal on 6, sequence ends</Text>
-              </View>
+                  <View style={styles.section}>
+                    <Checkbox 
+                    style={styles.checkbox} 
+                    value={isAlsoChecked} 
+                    onValueChange={setisAlsoChecked} />
+                    <Text style={styles.paragraph}>D3 Mortal on 6, sequence ends</Text>
+                    <Checkbox 
+                    style={styles.checkbox} 
+                    value={isChecked} 
+                    onValueChange={setChecked} />
+                  </View>
 
-              <View style={styles.section}>
-                <Checkbox 
-                style={styles.checkbox} 
-                value={isAlsoChecked} 
-                onValueChange={setisAlsoChecked} />
-                <Text style={styles.paragraph}>D3 Mortal on 6, continue roll</Text>
-              </View>
+                  <View style={styles.section}>
+                    <Checkbox 
+                    style={styles.checkbox} 
+                    value={isAlsoChecked} 
+                    onValueChange={setisAlsoChecked} />
+                    <Text style={styles.paragraph}>D3 Mortal on 6, continue roll</Text>
+                    <Checkbox 
+                    style={styles.checkbox} 
+                    value={isChecked} 
+                    onValueChange={setChecked} />
+                  </View>
 
-              <View style={styles.section}>
-                <Checkbox
-                  style={styles.checkbox}
-                  value={isChecked}
-                  onValueChange={setChecked}
-                  color={isChecked ? '#4630EB' : undefined}
-                />
-                <Text style={styles.paragraph}>Plus:  </Text> 
-                <TextInput
-                style={styles.textInputStyling}
-                placeholder='0'></TextInput>
-                <Text> to hit,</Text>
-                <TextInput
-                style={styles.textInputStyling}
-                placeholder='0'></TextInput>
-                <Text> to wound</Text>
-              </View>
+                  <View style={styles.section}>
+                    <Checkbox
+                      style={styles.checkbox}
+                      value={isChecked}
+                      onValueChange={setChecked}
+                      color={isChecked ? '#4630EB' : undefined}
+                    />
+                    <Text style={styles.paragraph}>Plus:</Text> 
+                    <TextInput
+                    style={styles.textInputStyling}
+                    placeholder='0'></TextInput>
+                    <Text>to hit,</Text>
+                    <TextInput
+                    style={styles.textInputStyling}
+                    placeholder='0'></TextInput>
+                    <Text> to wound</Text>
+                    <Checkbox 
+                    style={styles.checkbox} 
+                    value={isChecked} 
+                    onValueChange={setChecked} />
+                  </View>
+
+
         </View>
-        
+        {/* <View style={styles.woundCheckboxContainer}>
+                          <View style={styles.woundCheckboxPositioning}>
+                                <View style={styles.woundCheckbox} >
+                                    <Checkbox 
+                                      //style={styles.woundCheckbox} 
+                                      value={isChecked} 
+                                      onValueChange={setChecked} />
+                                </View>
+                                <View>
+                                    <Checkbox 
+                                      style={styles.woundCheckbox} 
+                                      value={isChecked} 
+                                      onValueChange={setChecked} />
+                                </View>
+                                <View>
+                                    <Checkbox 
+                                      style={styles.woundCheckbox} 
+                                      value={isChecked} 
+                                      onValueChange={setChecked} />
+                                </View>
+                                <View>
+                                    <Checkbox 
+                                      style={styles.woundCheckbox} 
+                                      value={isChecked} 
+                                      onValueChange={setChecked} />
+                                </View>
+                                <View>
+                                    <Checkbox 
+                                      style={styles.woundCheckbox} 
+                                      value={isChecked} 
+                                      onValueChange={setChecked} />
+                                </View>
+                                <View>
+                                    <Checkbox 
+                                      style={styles.woundCheckbox} 
+                                      value={isChecked} 
+                                      onValueChange={setChecked} />
+                                </View>
+                          </View>   
+         </View>
+                           */}
+                          
 
+         
+         
       </View>
     );
   }
@@ -106,26 +147,30 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      marginHorizontal: 16,
-      marginVertical: 32,
-      flexDirection: "row",
+     
+     
+      
     },
     section: {
       flexDirection: 'row',
       alignItems: 'center',
+      justifyContent: "space-between"
     },
     paragraph: {
       fontSize: 15,
+     
     },
     checkbox: {
-      margin: 8,
+     margin: 10,    
+     padding: 10,
     },
     textInputStyling: {
       textAlign: "center",
      
       
       
-    }
+    },
+   
   });
 
   export default CheckBoxGroup
