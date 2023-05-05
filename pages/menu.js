5db75fac6998e2fa10ccc11847bfdb3510fa97a8
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-05-05 23:34:08 +0100

    changing the menu page, adding a key to explain values in app. Damage page too crowded, need to change so adding a key to menu.

--- pages/menu.js
+++ pages/menu.js
@@ -7,9 +7,43 @@ function Menu({navigation}) {
   return (
 
     <View style={styles.mainContainer}>
-      
+          <View style={styles.keyContainer}>
 
-      
+            <View style={styles.keyPositioningLeft}>
+
+              <Text>Key:</Text>
+                <View style={styles.keyContentPositioning}>
+                  <Text style={styles.keyContent}>H</Text> 
+                  <Text>:To hit</Text>
+                </View>
+
+              <View style={styles.keyContentPositioning}>
+              <Text style={styles.keyContent}>W</Text>
+               <Text>:To wound</Text> 
+              </View>
+
+
+              <View style={styles.keyContentPositioning}>
+              <Text style={styles.keyContent}>A</Text>
+               <Text>:Attacks total</Text> 
+              </View>
+
+            </View>
+
+
+            <View style={styles.keyPositioningRight}>
+
+              <Text>R:</Text>
+              <Text>D:</Text>
+              <Text>S:</Text>
+              <Text>W:</Text>
+
+            </View>
+
+
+          </View>
+
+          <View style={styles.buttonContainer}>
               <TouchableOpacity 
               title='AvgDamage'
               onPress={()=> navigation.navigate("AvgDamage")} >
@@ -28,6 +62,8 @@ function Menu({navigation}) {
                 <Text style={styles.buttonContainerStyling}>DICE ROLLING</Text>
               </TouchableOpacity>
 
+            
+            </View>
              
        
 
@@ -39,6 +75,16 @@ function Menu({navigation}) {
 
 const styles = StyleSheet.create ({
   mainContainer: {
+    flex: 1,
+    
+  },
+
+  keyContainer: {
+    flex: 0.5,
+    flexDirection: "row"
+  },
+
+  buttonContainer: {
     flex: 1,
     flexDirection: "column",
     alignItems: "center",
@@ -55,7 +101,35 @@ const styles = StyleSheet.create ({
     fontWeight: "bold",
     width: 180,
     fontSize: 20,
-  }
+  },
+  keyPositioningLeft: {
+    backgroundColor: "lightgreen",
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  keyPositioningRight: {
+    flex: 1,
+    backgroundColor: "salmon",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+
+  },
+  keyContent: {
+    backgroundColor: "red",
+    borderRadius: 5,
+    padding: 8,
+    borderWidth: 4,
+    height: 30,
+    width: 30,
+    
+  },
+  
+  keyContentPositioning: {
+    flexDirection: "row"
+  },
 })
 
 export default Menu
